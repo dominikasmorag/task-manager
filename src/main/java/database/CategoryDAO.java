1f62b126bb4a24d0cab74dffb76d6c2c2df25ece
= <dominikasmorag@gmail.com>
2023-12-19 21:25:11 +0100

    method getCategoryByName returns Category object. if there is no such a category then new category is created

--- src/main/java/database/CategoryDAO.java
+++ src/main/java/database/CategoryDAO.java
@@ -1,14 +1,12 @@
 package database;
 
-import org.h2.jdbc.JdbcSQLIntegrityConstraintViolationException;
+import org.h2.jdbc.JdbcSQLNonTransientException;
 import task.Category;
 
 import java.sql.*;
 import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.Optional;
 
-import static java.lang.System.err;
 
 public class CategoryDAO implements DAO<Category> {
 
@@ -16,7 +14,7 @@ public class CategoryDAO implements DAO<Category> {
     private static final String INSERT_QUERY = "INSERT INTO categories (name, creationDate) VALUES (?, ?)";
     private PreparedStatement insertPrepStatement;
 
-    public CategoryDAO(Connection connection) throws SQLException {
+    public CategoryDAO(Connection connection) {
         this.connection = connection;
     }
     @Override
@@ -25,29 +23,46 @@ public class CategoryDAO implements DAO<Category> {
     }
 
     @Override
-    public void insert(Category category) throws SQLException {
-        category.setCreationDate(LocalDateTime.now());
-        insertPrepStatement =  connection.prepareStatement(INSERT_QUERY);
-        insertPrepStatement.setString(1, category.getCategoryName());
-        insertPrepStatement.setTimestamp(2, Timestamp.valueOf(LocalDateTime.now()));
-
+    public void insert(Category category){
         try {
+            category.setCreationDate(LocalDateTime.now());
+            insertPrepStatement = connection.prepareStatement(INSERT_QUERY);
+            insertPrepStatement.setString(1, category.getCategoryName());
+            insertPrepStatement.setTimestamp(2, Timestamp.valueOf(LocalDateTime.now()));
             insertPrepStatement.executeUpdate();
-        } catch (JdbcSQLIntegrityConstraintViolationException ex) {
-            System.err.println("Unique index or primary key violation");
+        } catch (SQLException ex) {
+            System.err.println("SQLException at 'insert' method in CategoryDAO");
         }
     }
 
     public int getIdByName(String name) throws SQLException {
-        String query = "SELECT id, name FROM categories WHERE name = ?";
+        String query = "SELECT id FROM categories WHERE name = ?";
         PreparedStatement statement = connection.prepareStatement(query);
         statement.setString(1, name);
         ResultSet rs = statement.executeQuery();
         rs.next();
-        int one = rs.getInt(1);
-        String two = rs.getString(2);
-        System.out.println("one: " + one + "\ntwo: " + two);
-        System.out.println("rs: " + rs);
-        return 10;
+        int id = rs.getInt(1);
+        return id;
+    }
+
+    public Category getCategoryByName(String name) {
+        Category category = new Category();
+        String query = "SELECT name FROM categories WHERE name = ?";
+        try {
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, name);
+            ResultSet rs = statement.executeQuery();
+            try {
+                rs.next();
+                category = new Category(rs.getString(1));
+            } catch (JdbcSQLNonTransientException ex) {
+                System.err.println("this category didn't exist but it is created now.");
+                category = new Category(name);
+                insert(category);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return category;
     }
 }
