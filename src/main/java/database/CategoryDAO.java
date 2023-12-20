ff5f62ab5dd43b0e9cad92ecceb54a54abc9ecf7
= <dominikasmorag@gmail.com>
2023-12-20 21:27:58 +0100

    methods renamed from 'get' to 'find'

--- src/main/java/database/CategoryDAO.java
+++ src/main/java/database/CategoryDAO.java
@@ -5,17 +5,21 @@ import task.Category;
 
 import java.sql.*;
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 
 public class CategoryDAO implements DAO<Category> {
-
+    public Map<String, Integer> categoriesMap;
     private Connection connection;
     private static final String INSERT_QUERY = "INSERT INTO categories (name, creationDate) VALUES (?, ?)";
     private PreparedStatement insertPrepStatement;
 
     public CategoryDAO(Connection connection) {
         this.connection = connection;
+        categoriesMap = updateMap();
+
     }
     @Override
     public Optional<Category> get(int id) {
@@ -27,15 +31,16 @@ public class CategoryDAO implements DAO<Category> {
         try {
             category.setCreationDate(LocalDateTime.now());
             insertPrepStatement = connection.prepareStatement(INSERT_QUERY);
-            insertPrepStatement.setString(1, category.getCategoryName());
+            insertPrepStatement.setString(1, category.getName());
             insertPrepStatement.setTimestamp(2, Timestamp.valueOf(LocalDateTime.now()));
             insertPrepStatement.executeUpdate();
         } catch (SQLException ex) {
             System.err.println("SQLException at 'insert' method in CategoryDAO");
+            ex.printStackTrace();
         }
     }
 
-    public int getIdByName(String name) throws SQLException {
+    public int findIdByName(String name) throws SQLException {
         String query = "SELECT id FROM categories WHERE name = ?";
         PreparedStatement statement = connection.prepareStatement(query);
         statement.setString(1, name);
@@ -45,7 +50,7 @@ public class CategoryDAO implements DAO<Category> {
         return id;
     }
 
-    public Category getCategoryByName(String name) {
+    public Category findByName(String name) {
         Category category = new Category();
         String query = "SELECT name FROM categories WHERE name = ?";
         try {
@@ -64,5 +69,20 @@ public class CategoryDAO implements DAO<Category> {
             ex.printStackTrace();
         }
         return category;
+    }
+
+     public Map<String, Integer> updateMap() {
+        HashMap<String, Integer> map = new HashMap<>();
+        String query = "SELECT name, id FROM categories";
+        try {
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                map.put(rs.getString(1), rs.getInt(2));
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return map;
     }
 }
