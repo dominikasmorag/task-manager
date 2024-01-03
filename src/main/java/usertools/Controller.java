0caa9e5090ee0f177e30b1a84a448417926b0c08
= <dominikasmorag@gmail.com>
2024-01-03 09:03:26 +0100

    some new options for user

--- src/main/java/usertools/Controller.java
+++ src/main/java/usertools/Controller.java
@@ -9,12 +9,10 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Controller {
-    private final Connection connection;
     private final TaskCreator taskCreator;
     private final TaskDAO taskDAO;
     private final TaskModifier taskModifier;
     public Controller(Connection connection) {
-        this.connection = connection;
         this.taskCreator = new TaskCreator(connection);
         this.taskDAO = new TaskDAO(connection);
         this.taskModifier = new TaskModifier(connection);
@@ -25,24 +23,41 @@ public class Controller {
         System.out.println("2 - show all tasks");
         System.out.println("3 - complete task");
         System.out.println("4 - show all tasks with LOW priority level");
+        System.out.println("5 - show all tasks with MEDIUM priority level");
+        System.out.println("6 - show all tasks with HIGH priority level");
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         switch(input) {
             case "1" : taskCreator.createTask();
-            break;
-            case "2" : taskDAO.findAll();
-            break;
+                break;
+            case "2" : List<TaskEntity> tasksList = taskDAO.findAll();
+                for(TaskEntity task : tasksList) {
+                    System.out.println(task);
+                }
+                break;
             case "3" : taskModifier.changeStatus();
-            break;
+                break;
             case "4" : List<TaskEntity> lowTasks = taskDAO.findAllWithField(PriorityLevel.LOW);
-            for(TaskEntity lowTask : lowTasks) {
+                for(TaskEntity lowTask : lowTasks) {
                 System.out.println(lowTask);
-            }
-            break;
+                }
+                break;
+            case "5" : List<TaskEntity> mediumTasks = taskDAO.findAllWithField(PriorityLevel.MEDIUM);
+                for(TaskEntity mediumTask : mediumTasks) {
+                    System.out.println(mediumTask);
+                }
+                break;
+            case "6" : List<TaskEntity> highTasks = taskDAO.findAllWithField(PriorityLevel.HIGH);
+                for(TaskEntity highTask : highTasks) {
+                    System.out.println(highTask);
+                }
+                break;
+
             default:
                 System.out.println("wrong number");
                 showPanel();
         }
+        showPanel();
     }
 
  }
