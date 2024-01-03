77137a019e60934bf03b80353c5352896a22f485
= <dominikasmorag@gmail.com>
2024-01-03 09:08:32 +0100

    changeStatus() method defended from entering wrong task's id

--- src/main/java/usertools/TaskModifier.java
+++ src/main/java/usertools/TaskModifier.java
@@ -8,32 +8,38 @@ import task.TaskEntity;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Timestamp;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class TaskModifier {
-    private final Connection conn;
     protected static TaskDAO taskDAO;
     protected static CategoryDAO categoryDAO;
 
     public TaskModifier(Connection conn) {
-        this.conn = conn;
         taskDAO = new TaskDAO(conn);
         categoryDAO = new CategoryDAO(conn);
+
     }
 
 
     public void changeStatus() {
         try {
             List<TaskEntity> tasks = taskDAO.findAllWithField(Status.PENDING);
+            Set<Integer> idSet = new HashSet<>();
             for(TaskEntity task : tasks) {
                 System.out.println("Id: " +task.getId() + ".\nTitle: " + task.getTitle() + ",\nDescription: " + task.getDescription() + ",\nDue date: " + task.getDueDate() + "\n============");
+                idSet.add(task.getId());
             }
             System.out.println("Enter id of desired task: ");
             Scanner sc = new Scanner(System.in);
-            sc.nextLine();
             int taskId = sc.nextInt();
-            TaskEntity taskEntity = taskDAO.findById(taskId).get();
+            TaskEntity taskEntity = new TaskEntity();
+            if(idSet.contains(taskId)) {
+                taskEntity = taskDAO.findById(taskId).get();
+            }
+            else {
+                System.out.println("There is no element with such id");
+                changeStatus();
+            }
             taskEntity.setStatus(Status.COMPLETED);
             taskDAO.updateStatus(taskEntity);
         } catch (SQLException ex) {
